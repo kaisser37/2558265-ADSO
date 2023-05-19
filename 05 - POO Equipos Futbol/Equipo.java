d0b80624f7854bfe95ecb026475d252aa0446a70
kaisser37 <santiagovergarafranco@gmail.com>
2023-05-19 11:05:51 -0500

    Version clase 19 may

---
+++ 05 - POO Equipos Futbol/Equipo.java
@@ -0,0 +1,15 @@
+public class Equipo{
+    int idEquipo;
+    int crearId = random (100 -300);
+    String nombreEquipo;
+    String tecnico;
+
+public Equipo (String nombreEquipo, String tecnico){
+this.nombreEquipo = nombre;
+this.tecnico = tecnico;
+this.idEquipo = crearId;
+}
+
+
+
+}
---
+++ 05 - POO Equipos Futbol/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int opcionMenu = 0
+        System.out.println("\n");
+		System.out.println("  +--------------------------------+");
+		System.out.println("  |        Menú principal          |");
+		System.out.println("  +--------------------------------+");
+		System.out.println("  |    1. Crear jugador            |");
+		System.out.println("  |    2. Crear equipo           |");
+		System.out.println("  |    3. Hacer traspaso            |");
+		System.out.println("  |    4. Ver listado de equipos           |");
+		System.out.println("  |    5. Ver detalle de equipos          |");
+		System.out.println("  |    6. Registrar partido             |");
+		System.out.println("  |    7. Ver detalle de los partidos   |");
+        System.out.println("  |    8. Ver listado de partidos jugados en un estadio                    |");
+        System.out.println("  |    9. Salir                    |");
+		System.out.println("  +--------------------------------+");
+
+		System.out.print("Ingrese una opción: ");
+		opcionMenu = entradaNumero.nextInt();
+		System.out.println("\n");
+    }
+
+}
