fc01f005138f4e3a606418c265b6b82ca8fed0fd
kaisser37 <santiagovergarafranco@gmail.com>
2023-05-19 07:56:38 -0500

    Nueva version 19-05

---
+++ 05 - POO Equipos Futbol/Jugador.java
@@ -0,0 +1,16 @@
+public class Jugador {
+    int cedula;
+    String nombres;
+    String apellidos;
+    boolean disponible = true;
+    String dorsal;
+    
+    public Jugador(int cedula, String nombres, String apellidos, String dorsal) {
+    this.cedula=cedula;
+    this.apellidos=apellidos;
+    this.nombres=nombres;
+    this.dorsal=dorsal;
+    }
+    
+}
+
--- Facturador POO/Facturador POO/Persona.java
+++ Facturador POO/Facturador POO/Persona.java
@@ -8,19 +8,6 @@ public class Persona {
         this.nombres = nombres;
         this.apellidos = apellidos;
     }
-
-    public int getCedula() {
-	    return this.cedula;
-	}
-
-    public String getNombres(){
-        return this.nombres;
-    }
-
-    public String getApellidos(){
-        return this.apellidos;
-    }
-
 }
 
 
